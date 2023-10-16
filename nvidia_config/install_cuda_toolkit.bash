e2edf532f61307559365617da2d168930b8c7fce
Mathis Mac <Mathis Mac>
2023-10-16 11:36:44 +0200

    Add firmware

--- nvidia_config/install_cuda_toolkit.bash
+++ nvidia_config/install_cuda_toolkit.bash
@@ -1,6 +1,9 @@
+apt-get install firmware-realtek
+
 wget https://developer.download.nvidia.com/compute/cuda/12.2.2/local_installers/cuda-repo-debian11-12-2-local_12.2.2-535.104.05-1_amd64.deb
-sudo dpkg -i cuda-repo-debian11-12-2-local_12.2.2-535.104.05-1_amd64.deb
-sudo cp /var/cuda-repo-debian11-12-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo add-apt-repository contrib
-sudo apt-get update
-sudo apt-get -y install cuda
+dpkg -i cuda-repo-debian11-12-2-local_12.2.2-535.104.05-1_amd64.deb
+cp /var/cuda-repo-debian11-12-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
+add-apt-repository contrib
+apt-get update
+apt-get -y install cuda
+
