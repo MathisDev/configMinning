ee5e5102522c07a0b6e66a0d388bab6a6981dc83
Mathis Mac <Mathis Mac>
2023-10-26 10:17:47 +0200

    Add Adresse

--- t-rex-0.26.8-linux/FIRO-2miners.sh
+++ t-rex-0.26.8-linux/FIRO-2miners.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./t-rex -a firopow -o stratum+tcp://firo.2miners.com:8181 -u aBR3GY8eBKvEwjrVgNgSWZsteJPpFDqm6U.rig0 -p x
+./t-rex -a firopow -o stratum+tcp://firo.2miners.com:8181 -u a3V9zpoPdP4d8fLSLTynTRG4qZ2BjwePEe.rig0 -p x
