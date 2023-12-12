fca2ec84e03139c561e7b860bde4980f7a62c33c
mathis <mathis@MacBook-Pro-de-mathis.local>
2023-12-12 21:22:50 +0100

    Config v1

--- demon_miner/miner_config/CFX-nanopool.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./t-rex -a octopus -o stratum+tcp://cfx-eu1.nanopool.org:17777 -u cfx:aajauymfc0cpd4aj91wmfyd150avfg3fmym9j2xrh8.rig0/your@email.org -p x -lock-cclock 1310 --mclock 1400 --fan 80 -Ihr-autotune-step-size 0.1 --Ihr-autotune-interval 5
